87f1ad084663a851078f1b14d4b91c6673d2fd38
Hussein Kasem <Hussein.Kasem@thetrainline.com>
2022-12-03 11:18:53 +0100

    Cleanup: Day 2 and make it kotlinish

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,48 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var total = 0
-        input.forEachIndexed { index, entry ->
-            val entries = entry.split(" ")
-            val opponent = parse(entries[0], false)
-            val mine = parse(entries[1], true)
-            val result = mine.play(opponent).value + mine.value
-            total += result
-        }
-        return total
+    fun part1(input: List<String>) = input.fold(0) { total, entry ->
+        val entries = entry.split(" ")
+        val opponent = parse(entries[0])
+        val mine = parse(entries[1])
+        total + mine.play(opponent).value + mine.value
     }
 
-    fun parseMoveForDesiredResult(suggestion: String, opponent: Move) =
-        when (decryptDesiredResult(suggestion)) {
-
-            Result.LOSS -> when (opponent) {
-                Move.ROCK____ -> Move.SCISSORS
-                Move.PAPER___ -> Move.ROCK____
-                Move.SCISSORS -> Move.PAPER___
-            }
-
-            Result.DRAW -> opponent
-
-
-            Result.WIN_ -> when (opponent) {
-                Move.ROCK____ -> Move.PAPER___
-                Move.PAPER___ -> Move.SCISSORS
-                Move.SCISSORS -> Move.ROCK____
-            }
-
-
-        }
-
-    fun part2(input: List<String>): Int {
-        var total = 0
-        input.forEach { entry ->
-            val entries = entry.split(" ")
-            val opponent = parse(entries[0], false)
-            val mine = parseMoveForDesiredResult(entries[1], opponent)
-            val result = mine.play(opponent).value + mine.value
-            total += result
-
-        }
-        return total
+    fun part2(input: List<String>) = input.fold(0) { total, entry ->
+        val entries = entry.split(" ")
+        val opponent = parse(entries[0])
+        val mine = opponent.moveForResult(parseDesiredResult(entries[1]))
+        total + mine.play(opponent).value + mine.value
     }
 
 // test if implementation meets criteria from the description, like:
@@ -55,34 +23,23 @@ fun main() {
     println(part2(input))
 }
 
-
-private fun parse(input: String, decrypt: Boolean): Move {
-    val value = if (decrypt) decrypt(input) else input
+private fun parse(value: String): Move {
     return when (value) {
-        "A" -> Move.ROCK____
-        "B" -> Move.PAPER___
-        "C" -> Move.SCISSORS
-        else -> error("Any")
+        "A", "X" -> Move.ROCK____
+        "B", "Y" -> Move.PAPER___
+        "C", "Z" -> Move.SCISSORS
+        else -> error("Err: $value")
     }
 }
 
-private fun decrypt(value: String) = when (value) {
-    "X" -> "A"
-    "Y" -> "B"
-    "Z" -> "C"
-    else -> error("Invalid")
+fun parseDesiredResult(value: String) = when (value) {
+    "X" -> Result.LOSS
+    "Y" -> Result.DRAW
+    "Z" -> Result.WIN_
+    else -> error("Err: $value")
 }
-
-fun decryptDesiredResult(value: String) =
-    when (value) {
-        "X" -> Result.LOSS
-        "Y" -> Result.DRAW
-        "Z" -> Result.WIN_
-        else -> error("Err")
-    }
 
 enum class Move(val value: Int) {
-
     ROCK____(1),
     PAPER___(2),
     SCISSORS(3);
@@ -95,10 +52,25 @@ enum class Move(val value: Int) {
             PAPER___ -> if (opponent == ROCK____) Result.WIN_ else Result.LOSS
         }
     }
+
+    fun moveForResult(result: Result) = when (result) {
+        Result.LOSS -> when (this) {
+            ROCK____ -> SCISSORS
+            PAPER___ -> ROCK____
+            SCISSORS -> PAPER___
+        }
+
+        Result.DRAW -> this
+        Result.WIN_ -> when (this) {
+            ROCK____ -> PAPER___
+            PAPER___ -> SCISSORS
+            SCISSORS -> ROCK____
+        }
+    }
+
 }
 
 enum class Result(val value: Int) {
-
     LOSS(0),
     DRAW(3),
     WIN_(6),
