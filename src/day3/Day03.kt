1b9fdde938dd267376e1176ee803e618e428d532
Hussein Kasem <Hussein.Kasem@thetrainline.com>
2022-12-03 12:59:24 +0100

    Day03: Cleanup

--- src/day3/Day03.kt
+++ src/day3/Day03.kt
@@ -3,51 +3,29 @@ package day3
 import readInput
 
 fun main() {
-    fun getType(compartment1: String, compartment2: String): Char {
-        var solution = ' '
-        for (item in compartment1) {
-            if (compartment2.contains(item)) {
-                solution = item
-            }
-        }
-        return solution
-    }
+    fun getType(compartment1: String, compartment2: String) = compartment1.first { compartment2.contains(it) }
 
-    fun getPriorityValue(type: Char) = if (type >= 'a') {
-        type - 'a' + 1
+    fun Char.toPriority() = if (this >= 'a') {
+        this - 'a' + 1
     } else {
-        type - 'A' + 1 + 26
+        this - 'A' + 1 + 26
     }
 
-    fun part1(input: List<String>): Int {
-        var total = 0
-        for (entry in input) {
-            val compartment1 = entry.substring(0 until entry.length / 2)
-            val compartment2 = entry.substringAfter(compartment1)
-            check(compartment1.length == compartment2.length)
-            val type = getType(compartment1, compartment2)
-            total += getPriorityValue(type)
-        }
-        return total
+    fun part1(input: List<String>): Int = input.fold(0) { total, entry ->
+        val compartment1 = entry.substring(0 until entry.length / 2)
+        val compartment2 = entry.substringAfter(compartment1)
+        val type = getType(compartment1, compartment2)
+        total + type.toPriority()
     }
 
-    fun getBadgeType(ruckstack1: String, ruckstack2: String, ruckstack3: String): Char {
-        var solution = ' '
-        for (item in ruckstack1) {
-            if (ruckstack2.contains(item) && ruckstack3.contains(item)) {
-                solution = item
-            }
-        }
-        return solution
-    }
+    fun getBadgeType(rucksack1: String, rucksack2: String, rucksack3: String) =
+        rucksack1.first { rucksack2.contains(it) && rucksack3.contains(it) }
 
     fun part2(input: List<String>): Int {
         var total = 0
         for (i in input.indices step 3) {
             val badgeType = getBadgeType(input.get(i), input.get(i + 1), input.get(i + 2))
-            total += getPriorityValue(badgeType)
-            println(badgeType)
-
+            total += badgeType.toPriority()
         }
         return total
     }
