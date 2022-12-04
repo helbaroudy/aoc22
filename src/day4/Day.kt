78e33e4621145d6e7c3569930a0f261aaa298300
Hussein Kasem <Hussein.Kasem@thetrainline.com>
2022-12-04 07:19:01 +0100

    Day04: Cleanup

--- src/day4/Day.kt
+++ src/day4/Day.kt
@@ -2,28 +2,42 @@ package day4
 
 import readInput
 
+typealias Section = Pair<Int, Int>
+
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun Section.contains(elf: Section) = this.first >= elf.first && this.second <= elf.second
+
+    fun Section.overlaps(elf: Section) = this.first >= elf.first && this.first <= elf.second
+
+    fun parse(entry: String): Pair<Section, Section> {
+        val rawElf1 = entry.split(",")[0]
+        val rawElf2 = entry.split(",")[1]
+        val elf1 = Section(
+            rawElf1.split("-")[0].toInt(),
+            rawElf1.split("-")[1].toInt()
+        )
+        val elf2 = Section(
+            rawElf2.split("-")[0].toInt(),
+            rawElf2.split("-")[1].toInt()
+        )
+        return Pair(elf1, elf2)
+    }
+
+    fun day4(input: List<String>, fullyContains: Boolean): Int {
         var total = 0
         for (entry in input) {
-            val rawElf1 = entry.split(",")[0]
-            val rawElf2 = entry.split(",")[1]
-            val elf1 = Pair(rawElf1.split("-")[0].toInt(), rawElf1.split("-")[1].toInt())
-            val elf2 = Pair(rawElf2.split("-")[0].toInt(), rawElf2.split("-")[1].toInt())
-
-            if (elf1.first >= elf2.first && elf1.second <= elf2.second ||
-                elf2.first >= elf1.first && elf2.second <= elf1.second ) total++
+            val (elf1, elf2) = parse(entry)
+            if (fullyContains) {
+                if (elf1.contains(elf2) || elf2.contains(elf1)) total++
+            } else if (elf1.overlaps(elf2) || elf2.overlaps(elf1)) {
+                total++
+            }
         }
         return total
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
-    val testInput = readInput("Day04_test")
-    part1(testInput)
-    check(part1(testInput) == 2)
+    fun part1(input: List<String>) = day4(input, true)
+    fun part2(input: List<String>) = day4(input, false)
 
     val input = readInput("Day04")
     println("Part 1: ${part1(input)}")
