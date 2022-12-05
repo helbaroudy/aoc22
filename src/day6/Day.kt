e9b98fc0eda739048e68f4e5472068d76ee50e89
Hussein Kasem <Hussein.Kasem@thetrainline.com>
2022-12-05 08:28:19 +0100

    Day06: Preparation

---
+++ src/day6/Day.kt
@@ -0,0 +1,21 @@
+package day6
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day06_test")
+    part1(testInput)
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day06")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
